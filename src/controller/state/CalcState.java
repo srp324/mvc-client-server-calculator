f4df1ee834ef1a7d584a9d3aea36851b618ea40b
sagarpatel <srp324@drexel.edu>
2018-03-16 17:31:50 -0400

    Implemented transitions

--- src/controller/state/CalcState.java
+++ src/controller/state/CalcState.java
@@ -5,6 +5,19 @@ import controller.Controller;
 public class CalcState implements State {
     @Override
     public void handle(Controller context) {
-        //TODO: Implement
+        if (isDigit(context.getCommand())) { //C8
+            context.getModel().equation = context.getCommand().getValue();
+            context.getModel().full += context.getCommand().getValue();
+            context.setState(new FirstOpState());
+        }
+        else { //C9
+            context.getModel().lhs = null;
+            context.getModel().rhs = null;
+            context.getModel().equation = "";
+            context.getModel().full = "";
+            context.getModel().clearNodes();
+            context.setState(new StartState());
+        }
+
     }
 }
