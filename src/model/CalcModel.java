e52204689b59f66f4ced2a1b116e8baea422a8b6
sagarpatel <srp324@drexel.edu>
2018-03-16 19:30:15 -0400

    Replaced CalcList with CalcModel

--- src/model/CalcModel.java
+++ src/model/CalcModel.java
@@ -1,8 +1,37 @@
 package model;
 
-//TODO: Replace CalcModel with CalcList
-public class CalcModel extends CalcList {
+public class CalcModel {
+    public String equation;
+    public String full; //TODO: Remove full
+    public Integer lhs;
+    public Integer rhs;
+    public String prevOp;
+    public String currOp;
+    TreeComponent tree;
+
     public CalcModel() {
-        super();
+        equation = "";
+        full = "";
+        prevOp = "";
+        currOp = "";
+    }
+
+    public TreeComponent getTree() {
+        return tree;
+    }
+    public void addNode(String op) {
+        tree = new Node(op, tree, null);
+    }
+    public void addNode(String op, String val, boolean lhs) {
+        if (lhs)
+            tree = new Node(op, new Leaf(val), null);
+        else
+            tree = new Node(op, null, new Leaf(val));
+    }
+    public void addRightLeaf(String digit) {
+        tree = new Node(((Node) tree).label, ((Node) tree).getLeft(), new Leaf(digit));
+    }
+    public void clearNodes() {
+        tree = v -> {};
     }
 }
